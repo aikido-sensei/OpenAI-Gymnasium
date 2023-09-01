30470ed864678462175d8f01e20ce78346335ba9
aikido-sensei <88054028+aikido-sensei@users.noreply.github.com>
2023-08-31 22:02:43 -0400

    Added the CarRacing category

---
+++ Box2D/CarRacing.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+import time
+
+env = gym.make("CarRacing", render_mode="human")
+
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    env.render()
+    if terminated or truncated:
+       observation, info = env.reset()
+
+env.close()
