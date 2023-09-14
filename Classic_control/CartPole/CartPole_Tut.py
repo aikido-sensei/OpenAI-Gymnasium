f26797cace926e2976a4e879db23ed943015a5db
aikido-sensei <88054028+aikido-sensei@users.noreply.github.com>
2023-09-14 17:57:04 -0400

    Current status

--- Classic_control/CartPole/CartPole_Tut.py
+++ Classic_control/CartPole/CartPole_Tut.py
@@ -2,27 +2,20 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv  # allows to use multiple environment at the same time
 from stable_baselines3.common.evaluation import evaluate_policy
+import os
 
 environment_name = "CartPole-v1"
-env = gym.make(environment_name, )
-episodes = 5
-for episode in range(1, episodes + 1):
-    state = env.reset()
-    done = False
-    score = 0
 
-    while not done:
-        env.render()
-        action = env.action_space.sample()
-        print(env.step(action))
-        n_state, reward, done, truncated, info = env.step(action)
-        score += reward
-    print('Episode:{} Score:{}'.format(episode, score))
 
 env = gym.make(environment_name, render_mode="human")
 env = DummyVecEnv([lambda: env])
 model = PPO('MlpPolicy', env, verbose = 1)
-model.learn(total_timesteps=20000)
+model.learn(total_timesteps=2000)
+
+PPO_path = os.path.join('Training', 'Saved Models', 'PPO_model')
+model.save(PPO_path)
+del model
+model = PPO.load('PPO_model', env=env)
 
 env.close()
 
